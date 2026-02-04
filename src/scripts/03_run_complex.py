890e8966263512ce4578f72b0c5995b869e4e2d1
Ghenntoggy1 <roman.gusev@isa.utm.md>
2026-02-04 04:07:41 +0200

    feat: implement function for working agents instantiation, add helper conversion function for question records into question mapping

--- src/scripts/03_run_complex.py
+++ src/scripts/03_run_complex.py
@@ -1,12 +1,14 @@
 import argparse
 import json
+import uuid
 from typing import Any, Optional, Union
 
 from loguru import logger
-from mlflow.genai.datasets import EvaluationDataset, get_dataset, search_datasets
+from mlflow.genai.datasets import EvaluationDataset, search_datasets  # get_dataset
 
 import mlflow
 
+from ..agents.pydantic_agent import WorkingAgent
 from ..config.make_config import make_config
 from ..datasets.loader import create_or_get_experiment
 
@@ -16,7 +18,12 @@ logger.info("Configuring defined variables...")
 # * ASSUMES:
 # * 1. USER PROMPT IS GIVEN (IN THIS CONTEXT, TAKEN DYNAMICALLY FROM EXPERIMENT DATASET)
 # * 2. SELECTED COMPLEXITY IS HIGH
-USER_PROMPT: str = ...
+USER_PROMPT: str = {
+    "dataset_record_id": uuid.uuid4(),
+    "inputs": {
+        "question": "What is the capital of Great Britain?"
+    }
+}
 IS_COMPLEX: bool = True
 
 # EXPERIMENT DATASET
@@ -25,8 +32,12 @@ MLFLOW_DATASET_NAME: str = "arena_hard_auto"
 
 # CONFIGURATION
 
+# WORKER PROMPT
+WORKING_AGENT_PROMPT: str = "You are a helpful AI assistant. Provide detailed, accurate answers."
+
 # CASCADE MODELS CONFIG
 CASCADE_MODELS_CONFIG: dict[str, str] = make_config()
+COMPLEX_RUN_CONFIG_KEY: str = "cascade_complex_run"
 
 # CASCADE LEVEL
 CASCADE_LEVEL: int = 1
@@ -77,14 +88,8 @@ def get_experiment_dataset(experiment_name: str = DATASET_EXPERIMENT_NAME, datas
             return dataset.to_dict()
     else:
         logger.warning(f"No dataset called: {dataset_name} found in MLFlow Experiment with Experiment ID: {experiment_id}")
-    
-    # # Previous version of dataset retrieval - not working with experiment_id
-    # dataset = get_dataset(dataset_id=experiment_id)
-    # records = dataset.to_dict()
-    
-    # return records
 
-def get_question_records_from_dataset(dataset: list, record_idx: Union[range, int]) -> Union[dict, list, None]:
+def get_question_records_from_dataset(dataset: list, record_idx: Union[range, int]) -> Union[list, None]:
     """Retrieve question record(s) from dataset by index or range.
 
     Args:
@@ -92,7 +97,7 @@ def get_question_records_from_dataset(dataset: list, record_idx: Union[range, in
         record_idx (Union[range, int]): Single index (int) or range of indices (range).
 
     Returns:
-        Union[dict, list, None]: list of record(s), None if invalid.
+        Union[list, None]: list of record(s), None if invalid.
     """
     dataset_size = len(dataset)
     
@@ -125,13 +130,13 @@ def get_question_records_from_dataset(dataset: list, record_idx: Union[range, in
         if record_idx.start < 0 or record_idx.stop > dataset_size:
             logger.error(
                 f"Index range: {record_idx} out of bounds. "
-                f"Dataset size: {dataset_size}. Returning available records."
+                f"Dataset size: {dataset_size}."
             )
             return None
         
         # Extract subset using range
         logger.success(f"Found question records in range: {record_idx}")
-        return dataset[record_idx.start:record_idx.step:record_idx.stop]
+        return dataset[record_idx.start:record_idx.stop:record_idx.step]
     
     # Handle invalid type
     else:
@@ -140,6 +145,18 @@ def get_question_records_from_dataset(dataset: list, record_idx: Union[range, in
             f"got: {type(record_idx).__name__}"
         )
         return None
+
+def question_records_to_question_str(question_records: list) -> dict[str, str]:
+    """Converts ArenaHardAuto question records to a mapping between question ID and question.
+
+    Args:
+        question_records (list): List of ArenaHardAuto question records.
+
+    Returns:
+        dict[str, str]: Converted question records to a mapping between question ID and question string value.
+    """
+    logger.info("Conveting question records to question mapping...")
+    return {record['dataset_record_id']: record['inputs']['question'] for record in question_records}
 
 def get_or_create_versioned_experiment(experiment_name: str) -> tuple[str, str, int]:
     """Find latest version of experiment for given complex workflow experiment, increment it if existent.
@@ -180,22 +197,25 @@ def get_or_create_versioned_experiment(experiment_name: str) -> tuple[str, str, 
     
     return experiment_id, experiment_name, next_version
 
+# TODO: check alternative models loading mechanism. Not sure if it is viable.
 # def load_models(config: dict[str, Any], config_key: str = "cascade_complex_run") -> dict[str, Any]:
 #     return config[config_key]
 
-def load_cascade_level_specific_models(config: dict[str, Any], cascade_level: int = 1, config_key: str = "cascade_complex_run") -> Optional[dict[str, Any]]:
+def load_cascade_level_specific_models(config: dict[str, Any], 
+                                       cascade_level: int, 
+                                       config_key: str) -> Optional[dict[str, Any]]:
     """Dynamically load from configuration file and extract specific cascade level models.
 
     Args:
         config (dict[str, Any]): Configuration file in dict format.
-        cascade_level (int, optional): Cascade level to look for models. Defaults to 1.
-        config_key (str, optional): Key of the complex cascade subset of models. Defaults to "cascade_complex_run".
+        cascade_level (int, optional): Cascade level to look for models.
+        config_key (str, optional): Key of the complex cascade subset of models.
 
     Returns:
-        Optional[dict[str, Any]]: Specific cascade models configuration at a specific cascade level. None if config entry or cascade models not found.
+        Optional[tuple[dict[str, Any], int]]: Specific cascade models configuration at a specific cascade level and corresponding cascade level as a tuple. None if config entry or cascade models not found.
     """
     logger.info(f"Retrieving cascade models configuration: {config_key} at level: {cascade_level}.")
-    cascade_models_config: dict[str, Any] = config.get(config_key, dict())
+    cascade_models_config: dict[str, Any] = config.get(config_key, {})
     
     if not cascade_models_config:
         logger.error(f"No config entry with key: {config_key}.")
@@ -211,6 +231,49 @@ def load_cascade_level_specific_models(config: dict[str, Any], cascade_level: in
     
     logger.success(f"Succesfully retrieved cascade models configuration at level: {cascade_level}")
     return cascade_models_config[key]
+
+
+def initialize_worker_agents(models_config: dict[str, dict[str, Any]], cascade_lvl: int) -> dict[str, WorkingAgent]: # Optional[dict[str, WorkingAgent]]:
+    """Initializes WorkingAgent objects at a specific level of cascade.
+
+    Args:
+        models_config (dict[str, dict[str, Any]]): Models config.
+        cascade_lvl (int): Specific level of cascade.
+
+    Returns:
+        dict[str, WorkingAgent]: Map of working agent ID and 
+    """
+    worker_agents_dict: dict[str, WorkingAgent] = {}
+    
+    for worker_key, worker_config in models_config.items():
+        logger.info(f"Attempting to instantiate working agent: {worker_key}")
+        model_name: str = worker_config.get("model_name", None)
+        # short_model_name: str = worker_config.get("short_model_name", "")
+        endpoint_struct: dict[str, str] = worker_config.get("endpoint", None)
+        parameters_struct: dict[str, Any] = worker_config.get("parameters", None)
+
+        if any(x is None for x in [model_name, endpoint_struct, parameters_struct]):
+            logger.warning("Malformed cascade model configuration found. Skipping...")
+            continue
+            # return None
+        
+        worker_agents_dict[worker_key] = WorkingAgent(
+            model_id=f"{model_name}/{uuid.uuid4()}", # uuid.uuid4(),
+            role_name=worker_key,
+            system_instruction=WORKING_AGENT_PROMPT,
+            cascade_tier=cascade_lvl,
+            config=worker_config, # parameters_struct,
+            api_key=endpoint_struct.get('api_key', None)
+        )
+        
+        logger.success(f"Instantiated working agent with ID: {worker_agents_dict[worker_key].model_id}")
+        
+    return worker_agents_dict
+
+async def run_cascade_loop(worker_agents: dict[str, WorkingAgent], prompts: dict[str, str], current_level: int = 1) -> None:
+    # TODO: implement
+    logger.error("Method not implemented.")
+    # raise NotImplementedError
 
 
 def main() -> None:
@@ -229,41 +292,51 @@ def main() -> None:
     
     # * SCENARIO 1: ENTIRE MLFLOW DATASET OF QUESTIONS
     # question_records_idx: int = range(0, len(dataset_records))
-    # question_records = get_questions_from_dataset(dataset=dataset_records, record_idx=range(0, len(dataset_records)))
+    # question_records: list = get_question_records_from_dataset(dataset=dataset_records, record_idx=question_records_idx)
     # print(question_records)
     # if not question_records:
     #     logger.error("Question records not found.")
     #     return
     
-    # * SCENARIO 2: SUBSET OF QUESTIONS FROM MLFLOW DATASET
+    # * SCENARIO 2: SUBSET OF QUESTIONS (10) FROM MLFLOW DATASET
     # question_records_idx: int = range(0, 11)
-    # question_records = get_questions_from_dataset(dataset=dataset_records, record_idx=range(0, 11))
-    # print(question_records)
+    # question_records: list = get_question_records_from_dataset(dataset=dataset_records, record_idx=question_records_idx)
     # if not question_records:
     #     logger.error("Question records not found.")
     #     return
     
     # * SCENARIO 3: CUSTOM QUESTION
-    # question_record = [USER_PROMPT]
+    # question_records: list = [USER_PROMPT]
     
     # * SCENARIO 4: SINGLE QUESTION FROM MLFLOW DATASET
     question_records_idx: int = 1
-    question_record = get_question_records_from_dataset(dataset=dataset_records, record_idx=question_records_idx)[0]
-    if not question_record:
+    question_records: list = get_question_records_from_dataset(dataset=dataset_records, record_idx=question_records_idx)
+    if not question_records:
         logger.error("Question record not found.")
         return
     
-    print(f"Question {question_records_idx}: {question_record['inputs']['question']}")
+    questions_mapping: dict[str, str] = question_records_to_question_str(question_records=question_records)
     
+    for idx, (question_id, question) in enumerate(questions_mapping.items()):
+        print(f"Question {idx + 1}: {question_id} - {question}")
+        
     # * STEP 3: Configuring Agents
+    
     # TODO: Uncomment experiment setting
     # experiment_id, experiment_name, version = get_or_create_versioned_experiment(experiment_name=EXPERIMENT_NAME)
     # mlflow.set_experiment(experiment_name)
     
-    cascade_lvl_models: dict[str, Any] = load_cascade_level_specific_models(config=CASCADE_MODELS_CONFIG)
+    # * Loading complex run config models
+    cascade_lvl_models = load_cascade_level_specific_models(config=CASCADE_MODELS_CONFIG, cascade_level=CASCADE_LEVEL, config_key=COMPLEX_RUN_CONFIG_KEY)
+    print(f"Current Cascade Level: {CASCADE_LEVEL}")
     print(json.dumps(cascade_lvl_models, indent=2))
     
+    # * Instantiate Agents using working models configs
+    worker_agents_dict: dict[str, WorkingAgent] = initialize_worker_agents(models_config=cascade_lvl_models, cascade_lvl=CASCADE_LEVEL)
+    for worker_key, worker_agent in worker_agents_dict.items():
+        print(worker_key, worker_agent.model_id)
     
+    run_cascade_loop(worker_agents=worker_agents_dict, prompts=questions_mapping, current_level=CASCADE_LEVEL)
     
 
 if __name__ == "__main__":
